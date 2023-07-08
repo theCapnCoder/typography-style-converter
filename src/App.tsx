cfb334bceccecd79c777f94e3d89aaa68606fc16
PavelM <myagkiypavel@gmail.com>
2023-07-08 19:27:49 +0300

    chore: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Converter } from "./containers/Converter";
+
 function App() {
   return (
     <div>
       <header>
         <h2>Converter</h2>
+        <Converter />
       </header>
     </div>
   );
