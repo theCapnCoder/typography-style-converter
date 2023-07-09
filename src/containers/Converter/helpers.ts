6d547060c541dc4bbcac8db3665c894aae6d72a7
PavelM <myagkiypavel@gmail.com>
2023-07-09 15:38:03 +0300

    chore: delete coments

--- src/containers/Converter/helpers.ts
+++ src/containers/Converter/helpers.ts
@@ -13,16 +13,10 @@ function transformCSSCode(cssCode: string): Record<string, string | number> {
     const [property, value] = line.split(':').map(part => part.trim());
     const transformedProperty = property.replace(/-(\w)/g, (_, letter) => letter.toUpperCase());
 
-    // styles[transformedProperty] = isQuotedProperty(transformedProperty) ? value.replace(/["']/g, '') : value;
     styles[transformedProperty] = transformValue(transformedProperty, value)
   });
 
   return styles;
 }
-
-function isQuotedProperty(property: string): boolean {
-  return ['color', 'textAlign', 'fontFamily', 'fontSize', 'fontStyle'].includes(property);
-}
-
 
 export default transformCSSCode;
