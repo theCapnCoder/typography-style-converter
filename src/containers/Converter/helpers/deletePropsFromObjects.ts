f05452d5a360c4d967e8d7e29b3bc2d2b4521f2b
PavelM <myagkiypavel@gmail.com>
2023-07-09 15:36:36 +0300

    chore: deletePropsFromObjects

--- src/containers/Converter/helpers/deletePropsFromObjects.ts
+++ src/containers/Converter/helpers/deletePropsFromObjects.ts
@@ -16,10 +16,10 @@ interface ObjectWithCount {
 
 export function deletePropsFromObjects(arr: ObjectWithCount[], propsToDelete: string[]): ObjectWithCount[] {
   const result: ObjectWithCount[] = [];
+  console.log(propsToDelete)
 
   for (const item of arr) {
     const obj = { ...item.object };
-    console.log({item, obj})
 
     for (const prop of propsToDelete) {
       delete obj[prop];
@@ -30,31 +30,3 @@ export function deletePropsFromObjects(arr: ObjectWithCount[], propsToDelete: st
 
   return result;
 }
-
-// Example usage:
-const objects: ObjectWithCount[] = [
-  {
-    "count": 1,
-    "object": {
-      "color": "#FFF",
-      "textAlign": "center",
-      "fontFamily": "Averta CY",
-      "fontSize": 39,
-      "fontStyle": "normal",
-      "fontWeight": 600,
-      "lineHeight": "103.1%"
-    }
-  },
-  {
-    "count": 1,
-    "object": {
-      "color": "#FFF",
-      "fontFamily": "Averta CY",
-      "fontSize": 16,
-      "fontStyle": "normal",
-      "fontWeight": 400,
-      "lineHeight": "148.6%"
-    }
-  }
-];
-
